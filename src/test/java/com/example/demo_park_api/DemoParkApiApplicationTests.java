74787019c5043d0b4596669a7c7a165efd304da5
Andre-MB <andrezinhogame15@gmail.com>
2024-08-20 08:59:19 -0300

    first commit

---
+++ src/test/java/com/example/demo_park_api/DemoParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_park_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
